f081a41ee4c482bd11cf4b68f476ef609a651e7d
Tanishq Thuse <tanishqthuse555@gmail.com>
2024-06-06 18:17:45 +0530

    E08

--- E08_sum_of_factorials.c
+++ E08_sum_of_factorials.c
@@ -1,7 +1,9 @@
 /*08 
 Write  a  C  program  to  find  the  sum  of  Natural 
 Number/Factorial of Number of all natural 
-numbers from 1 to N*/
+numbers from 1 to N
+Series: 1/1! + 2/2! + 3/3! + 4/4! + ... N/N!
+*/
 
 #include<stdio.h>
 
@@ -18,9 +20,12 @@ int main(){
     printf("Enter the number : ");
     int n;
     scanf("%d",&n);
-    int sum = 0;
+    // long double sum = 0;
+    int sum = 0 ;
     for(int i=1 ; i<= n; i++){
-        sum += fact(i);
+        // sum += i/fact(i);
+        sum = sum + fact(i);
     }
-    printf("The sum of facts from 1 to %d is %d\n", n, sum);
+    // printf("The sum of facts from 1 to %d is %llf and size %d\n", n, sum, sizeof(sum));
+    printf("The sum of facts from 1 to %d is %d and size %d\n", n, sum, sizeof(sum));
 }
